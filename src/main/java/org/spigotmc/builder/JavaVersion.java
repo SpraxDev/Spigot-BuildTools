9eb453142e87eab36cb65f166ee3548979e75405
md_5 <git@md-5.net>
2019-03-17 11:48:53 +1100

    Prepare for imminent Java 12 release

--- src/main/java/org/spigotmc/builder/JavaVersion.java
+++ src/main/java/org/spigotmc/builder/JavaVersion.java
@@ -17,6 +17,7 @@ public class JavaVersion
     public static final JavaVersion JAVA_9 = new JavaVersion( "Java 9", 53 );
     public static final JavaVersion JAVA_10 = new JavaVersion( "Java 10", 54 );
     public static final JavaVersion JAVA_11 = new JavaVersion( "Java 11", 55 );
+    public static final JavaVersion JAVA_12 = new JavaVersion( "Java 12", 56 );
     //
     private final String name;
     private final int version;
