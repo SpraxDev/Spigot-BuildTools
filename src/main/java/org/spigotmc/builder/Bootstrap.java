605754c581bc39d14c34cf155f00a91382b0c5ca
Christian Koop <contact@sprax2013.de>
2021-04-01 19:46:10 +0200

    Apply remote commit (based on original build 126)
    >  Prepare for Java 16
    
    https://hub.spigotmc.org/stash/projects/SPIGOT/repos/buildtools/commits/36c11e9f624ef7482286482948327eb428f81b4d

--- src/main/java/org/spigotmc/builder/Bootstrap.java
+++ src/main/java/org/spigotmc/builder/Bootstrap.java
@@ -29,7 +29,7 @@ public class Bootstrap {
      * This field holds the compatibility information
      * When the original project updates, this field has to be updated too!
      */
-    public static final int ORIGINAL_BUILD_NUMBER = getBuildNumber("git-BuildTools-a56c4cb-125");
+    public static final int ORIGINAL_BUILD_NUMBER = getBuildNumber("git-BuildTools-36c11e9-126");
 
     public static final boolean IS_WINDOWS = System.getProperty("os.name").startsWith("Windows");
     public static final boolean AUTO_CRLF = !"\n".equals(System.getProperty("line.separator"));
@@ -178,7 +178,7 @@ public class Bootstrap {
 
         if (javaVersion.isUnknown()) {
             System.err.println("*** WARNING *** Unsupported Java detected (" + System.getProperty("java.class.version") +
-                    "). BuildTools has only been tested up to Java 15. Use of development Java versions is not supported.");
+                    "). BuildTools has only been tested up to Java 16. Use of development Java versions is not supported.");
             System.err.println("*** WARNING *** You may use 'java -version' to double check your Java version.");
         } else {
             // Older JVMs (including Java 8) report less than Xmx here. Allow some slack for people actually using -Xmx512M
--- src/main/java/org/spigotmc/builder/JavaVersion.java
+++ src/main/java/org/spigotmc/builder/JavaVersion.java
@@ -18,6 +18,7 @@ public class JavaVersion {
     public static final JavaVersion JAVA_13 = new JavaVersion("Java 13", 57);
     public static final JavaVersion JAVA_14 = new JavaVersion("Java 14", 58);
     public static final JavaVersion JAVA_15 = new JavaVersion("Java 15", 59);
+    public static final JavaVersion JAVA_16 = new JavaVersion("Java 16", 60);
 
     private final String name;
     private final int version;
